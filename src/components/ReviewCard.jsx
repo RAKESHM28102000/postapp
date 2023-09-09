fea92f0711a2c407308f39fe19ec82f80829fdbd
RAKESHM28102000 <mrakeshmrakesh885@gmail.com>
2023-09-09 12:33:47 +0530

    tooltip updated

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,5 +1,4 @@
 import  React, { useState } from 'react';
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
@@ -7,6 +6,7 @@ import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
 
 // import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
@@ -17,25 +17,25 @@ import Favorite from '@mui/icons-material/Favorite';
 import SnackBar from './SnackBar';
 
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+// const ExpandMore = styled((props) => {
+//   const { expand, ...other } = props;
+//   return <IconButton {...other} />;
+// })(({ theme, expand }) => ({
+//   transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+//   marginLeft: 'auto',
+//   transition: theme.transitions.create('transform', {
+//     duration: theme.transitions.duration.shortest,
+//   }),
+// }));
 
 export default function ReviewCard({title,content,userid,handleDelete}) {
-  const [expanded, setExpanded] =useState(false);
+  // const [expanded, setExpanded] =useState(false);
   const [like,setLike]=useState(0);
   
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+  // const handleExpandClick = () => {
+  //   setExpanded(!expanded);
+  // };
 
   function handleLike(){
     setLike((prev)=>prev+1);
@@ -59,16 +59,22 @@ export default function ReviewCard({title,content,userid,handleDelete}) {
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites" onClick={handleLike} >
-          <Checkbox  icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{color:"red"}}/>} /> 
-          {like >=1 ? (like):''}
-          </IconButton>
-          <IconButton aria-label="comment">
-              <DailogBox comment={content}/>
-          </IconButton>
+          <Tooltip title="Like">
+             <IconButton aria-label="add to favorites" onClick={handleLike} >
+              <Checkbox  icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{color:"red"}}/>} /> 
+              {like >=1 ? (like):''}
+             </IconButton>
+          </Tooltip>
+          <Tooltip title="comment">
+              <IconButton aria-label="comment">    
+                <DailogBox comment={content}/>
+             </IconButton>
+          </Tooltip>
+          <Tooltip title="delete">
           <IconButton aria-label="delete" >
              <SnackBar handleDelete={handleDelete} userid={userid}/>
           </IconButton>
+          </Tooltip>
         {/* <ExpandMore
           expand={expanded}
           onClick={handleExpandClick}
